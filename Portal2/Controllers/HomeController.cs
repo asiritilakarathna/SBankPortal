e57162acc267eccfa621ea38b802209380c45c63
asiritilakarathna <nuwanasiri@live.com>
2017-01-23 02:10:40 +0530

    aaa

--- Portal2/Controllers/HomeController.cs
+++ Portal2/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.SharePoint.Client;
+using System.Web.Script.Serialization;
 
 namespace Portal2.Controllers
 {
@@ -10,6 +12,31 @@ namespace Portal2.Controllers
     {
         public ActionResult Index()
         {
+            Uri siteUri = new Uri("https://nuwantilakarathna.sharepoint.com");
+            string realm = TokenHelper.GetRealmFromTargetUrl(siteUri);
+
+            string accessToken = TokenHelper.GetAppOnlyAccessToken(TokenHelper.SharePointPrincipal,
+                                                                    siteUri.Authority, realm).AccessToken;
+
+            using (var clientContext = TokenHelper.GetClientContextWithAccessToken(siteUri.ToString(), accessToken))
+            {
+                Web currentWeb = clientContext.Web;
+                var list = clientContext.Web.Lists.GetByTitle("ImageRotatorContents");
+                CamlQuery camlQuery = new CamlQuery();
+                camlQuery.ViewXml = "<View><RowLimit>100</RowLimit></View>";
+
+                //Use the fully qualified name to disambiguate the ListItemCollection type.
+                Microsoft.SharePoint.Client.ListItemCollection listItems = list.GetItems(camlQuery);
+                clientContext.Load<List>(list);
+                clientContext.Load<Microsoft.SharePoint.Client.ListItemCollection>(listItems);
+
+                clientContext.ExecuteQuery();
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                ViewData["JsonRegionList"] = serializer.Serialize(listItems);
+            }
+
+            Console.WriteLine("...");
+            Console.ReadLine();
             return View();
         }
 
